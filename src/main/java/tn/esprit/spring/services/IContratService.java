be32cf373a665b2fbb7aa2b34f5e007a56d55a9e
louayyahyaoui <louay.yahyaoui@esprit.tn>
2021-11-06 23:56:18 +0100

    Add test methods (ContratTest.java) for the entity Contrat

---
+++ src/main/java/tn/esprit/spring/services/IContratService.java
@@ -0,0 +1,9 @@
+package tn.esprit.spring.services;
+
+import tn.esprit.spring.entities.Contrat;
+import tn.esprit.spring.entities.Employe;
+
+public interface IContratService {
+
+    public int ajouterContrat(Contrat contrat);
+}
---
+++ src/test/java/tn/esprit/spring/test/ContratTest.java
@@ -0,0 +1,59 @@
+package tn.esprit.spring.test;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import tn.esprit.spring.entities.Contrat;
+
+import tn.esprit.spring.services.EmployeServiceImpl;
+
+
+import java.util.Date;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ContratTest {
+    private static final Logger L = LogManager.getLogger(EmployeTest.class);
+    @Autowired
+    EmployeServiceImpl EmplService;
+    int referenceContrat;
+
+    @Before
+    public void initBefore() {
+
+        L.info("startup - Add a new Conrat");
+        Date current = new Date();
+        L.info("Create Date - Create Current Date");
+        Contrat contrat= new Contrat(current,"CDD",2000);
+        referenceContrat=EmplService.ajouterContrat(contrat);
+        L.info("Finish - Contrat added in @Before with reference ==> : " + referenceContrat);
+    }
+
+    @Test
+    public void initTest() {
+
+        L.info("startup - Add a new Conrat");
+        Date current = new Date();
+        Contrat contrat= new Contrat(current,"CDD",2000);
+        referenceContrat=EmplService.ajouterContrat(contrat);
+        L.info("Finish - Contrat added in @Test with reference ==> : " + referenceContrat);
+    }
+
+    @After
+    public void initAfter() {
+
+        L.info("startup - Add a new Conrat");
+        Date current = new Date();
+        Contrat contrat= new Contrat(current,"CDD",2000);
+        referenceContrat=EmplService.ajouterContrat(contrat);
+        L.info("Finish - Contrat added in @After with reference ==> : " + referenceContrat);
+    }
+}
