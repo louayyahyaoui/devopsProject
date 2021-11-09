febfbbae82d9d5dccf7f6bc96a7af3b7f303839a
HamzaEnnour <49129326+HamzaEnnour@users.noreply.github.com>
2021-11-09 16:24:58 +0100

    adding & fixing Tests

--- src/test/java/tn/esprit/spring/test/MissionTest.java
+++ src/test/java/tn/esprit/spring/test/MissionTest.java
@@ -10,7 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-
+import tn.esprit.spring.entities.Departement;
+import tn.esprit.spring.entities.Mission;
+import tn.esprit.spring.services.EntrepriseServiceImpl;
 import tn.esprit.spring.services.MissionServiceImpl;
 
 
@@ -19,39 +21,40 @@ import tn.esprit.spring.services.MissionServiceImpl;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MissionTest {
-    private static final Logger L = LogManager.getLogger(EmployeTest.class);
+    private static final Logger L = LogManager.getLogger(MissionTest.class);
     @Autowired
     MissionServiceImpl MissionService;
+    @Autowired
+	EntrepriseServiceImpl EntrpService;
+    Departement idDep;
     int referenceMission;
 
     @Before
     public void initBefore() {
 
         L.info("startup - Add a new Mission");
+        Departement departemet = new Departement("devops project");
+		idDep=EntrpService.ajouterDepartement(departemet);
+		Mission miss = new Mission("mission one", "this is a description of mission one") ;
+		referenceMission = MissionService.addMission(miss);
 
     }
 
     @Test
-    public void initTest() {
+    public void GetMisionTest() {
 
-        L.info("startup - Add a new Mission");
-
+        L.info("startup - get current mission added");
+        Mission tempMiss= MissionService.getMissionById(referenceMission);
+        L.info("Info - Current Mission : ",tempMiss);
     }
 
-    @After
-    public void initAfter() {
-
-        L.info("startup - Add a new Mission");
-
-    }
 
     @Test
-    public void deleteTestContrat() {
+    public void deleteMissionTest() {
 
-        try {
-            L.info("----Start Test--- ");
-
-        }
-        catch (Exception e) { L.error("Erreur : " + e); }
+            L.info("----Start Test--- Deleting Mission ");
+            MissionService.deleteMissionById(referenceMission);
+            L.info("----End Test--- ");
+       
     }
 }
